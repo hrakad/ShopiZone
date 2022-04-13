d11adf32ba01e01916cab18ad5d7efd4ed4e118a
hrakad <h.alrakad82@hotmail.com>
2022-04-13 11:30:28 -0600

    Merge branch 'master' of https://github.com/hrakad/ShopiZone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,23 @@
-import data from "./data";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">ShopiZone</a>
-      </header>
-      <main>
-        <h1> Products</h1>
-        <div className="products">
-          {
-            data.products.map(product => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">ShopiZone</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
 
-                <div className="product_info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>
-                      {product.name}
-                    </p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to Cart</button>
-                </div>
-
-              </div>
-            )
-
-            )
-          }
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
