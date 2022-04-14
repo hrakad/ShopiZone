a8838efa9bc16a5adde100ba1a6a2e4bcb7d307b
hrakad <h.alrakad82@hotmail.com>
2022-04-14 17:23:52 -0600

    Create server and install dependencies

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data = {
+  products: [
+    {
+      name: "Bean Spaghetti Pasta",
+      slug: "bean-spaghetti-pasta",
+      category: "Organic Beans",
+      image: "/images/bean pasta.jpg",
+      price: 27.99,
+      countInStock: 300,
+      brand: "EXPLORE CUISINE",
+      flavor: "Black bean",
+      rating: 4.8,
+      numReviews: 227,
+      description: "Gluten Free Pasta, Easy to Make - USDA Certified Organic, Vegan, Kosher, Non GMO"
+    },
+    {
+      name: "Plant-Based Protein Powder",
+      slug: "plant-based-protein-powder",
+      category: "Protein",
+      image: "/images/protein powder.jpg",
+      price: 24.99,
+      countInStock: 200,
+      brand: "MRM",
+      flavor: "Chocolate Mocha",
+      rating: 4.5,
+      numReviews: 1337,
+      description: "Easy to Digest | with BCAAs| Vegan + Gluten-Free | Clinically Tested| Digestive enzymes"
+    },
+    {
+      name: "Plant-Based Protein Bars",
+      slug: "plant-based-protein-bar",
+      category: "Protein",
+      image: "/images/protein bar.jpg",
+      price: 40.95,
+      countInStock: 150,
+      brand: "Garden of Life",
+      flavor: "Chocolate Fudge ",
+      rating: 4.6,
+      numReviews: 397,
+      description: "Organic Fit Bar, Chocolate Fudge (12 per carton) - Burn Fat, Satisfy Hunger and Fight Cravings, Low Sugar Plant Protein Bar with Fiber"
+    },
+    {
+      name: "Stevia",
+      slug: "stevia",
+      category: "Organic Sugar",
+      image: "/images/stevia.jpg",
+      price: 7.99,
+      countInStock: 900,
+      brand: "Stevia Select",
+      flavor: "Stevia",
+      rating: 4.3,
+      numReviews: 356,
+      description: "Sugar Free Plant Based Stevia Sweetener USDA Certified | Non GMO Pure Stevia Extract from Sweet Leaf"
+    }, {
+      name: "Organic Peanut Butter",
+      slug: "organic-peanut-butter",
+      category: "Organic Beans",
+      image: "/images/peanut butter.jpg",
+      price: 27.99,
+      countInStock: 120,
+      brand: "WOODSTOCK",
+      flavor: "Peanut",
+      rating: 4.0,
+      numReviews: 7,
+      description: "Certified Kosher Smooth, creamy, and unsalted, Only 1 ingredient"
+    },
+    {
+      name: "Nutritional Yeast Powder",
+      slug: "nutritional-yeast-powder",
+      category: "Vitamins",
+      image: "/images/yeast powder.jpg",
+      price: 33.24,
+      countInStock: 90,
+      brand: "NOW",
+      flavor: "N/A",
+      rating: 4.2,
+      numReviews: 92,
+      description: "Non-GMO, Vegan/Vegetarian, Kosher, Soy Free, Sugar Free, Dairy Free"
+    }
+  ]
+
+}
+
+export default data;
