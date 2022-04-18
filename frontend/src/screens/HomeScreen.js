9517d5551b1d7ee7b047dbb8ddba46ebbb8db543
hrakad <h.alrakad82@hotmail.com>
2022-04-18 13:24:06 -0600

    Fetch products from backend by axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,38 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from 'axios';
 
 function HomeScreen() {
-  return <div><h1> Products</h1>
-    <div className="products">
-      {
-        data.products.map(product => (
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Products</h1>
+      <div className="products">
+        {products.map(product => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </Link>
-
             <div className="product_info">
               <Link to={`/product/${product.slug}`}>
-                <p>
-                  {product.name}
-                </p>
+                <p>{product.name}</p>
               </Link>
               <p>
                 <strong>${product.price}</strong>
               </p>
               <button>Add to Cart</button>
             </div>
-
           </div>
-        )
-
-        )
-      }
-    </div></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default HomeScreen;
