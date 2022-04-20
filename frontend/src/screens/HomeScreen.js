3cce1320fbcad5bbc309bf45bfb48f8ae1882289
hrakad <h.alrakad82@hotmail.com>
2022-04-19 23:52:52 -0600

    Create reducer function

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,18 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL';
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
 function HomeScreen() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
