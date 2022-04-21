2dc6a7f95e73884f67c34c5340fc1d47e44ef6d8
hrakad <h.alrakad82@hotmail.com>
2022-04-21 15:45:26 -0600

    Create navbar and footer and add style using react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,37 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">ShopiZone</Link>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>ShopiZone</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
-        <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
 
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">
+            © 2022, Shopyzone.com, Inc.
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
