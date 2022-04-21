3bd11b163927c1352afda77fe80e273d17b6bfc8
hrakad <h.alrakad82@hotmail.com>
2022-04-21 11:14:18 -0600

    Update fetch data and get state from useReducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const reducer = (state, action) => {
       return { ...state, loading: true };
     case 'FETCH_SUCCESS':
       return { ...state, products: action.payload, loading: false };
-    case 'FETCH_FAIL';
+    case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
@@ -16,14 +16,22 @@ const reducer = (state, action) => {
 }
 function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
     loading: true,
     error: '',
   })
   //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message })
+      }
+
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
