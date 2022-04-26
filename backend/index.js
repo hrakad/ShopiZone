e06eac5cb4ae3b10bf884343428e98cc653f5a82
hrakad <h.alrakad82@hotmail.com>
2022-04-25 18:57:48 -0600

    Create new route to display the product details

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find(x => x.slug === req.params.slug);
+  if (product) {
+    res.send(product)
+  } else {
+    res.send(404).send({ message: 'Product Not Found' });
+  }
+  res.send(data.products);
+});
+
 const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
