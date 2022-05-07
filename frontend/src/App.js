eafb823ed7964e672cd80034b00b59924f7971a7
hrakad <h.alrakad82@hotmail.com>
2022-05-07 17:44:21 -0600

    Add signup route at app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -65,6 +66,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
